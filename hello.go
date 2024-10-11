b904b1a58641876ae5e50fd1c1bbeb9c8bc2f039
kul-sudo <ulyanov.kirill@protonmail.com>
2024-10-11 05:17:07 +0300

    grid

--- hello.go
+++ hello.go
@@ -8,8 +8,8 @@ import (
 type Piece uint8
 
 const (
-	NAME string = "Lichess TUI"
-	CELL_SIZE int = 8
+	NAME      string = "Lichess TUI"
+	CELL_SIZE int    = 1
 )
 
 const (
@@ -22,24 +22,22 @@ const (
 )
 
 func newPrimitive(text string) tview.Primitive {
-	return tview.NewTextView().
-		SetTextAlign(tview.AlignCenter).
-		SetText(text)
+	return tview.NewBox().SetBorder(true)
 }
 func main() {
-	menu := newPrimitive("Menu")
+	// menu := newPrimitive("Menu")
 	// main := newPrimitive("Main content")
 	// sideBar := newPrimitive("Side Bar")
 	//
 	grid := tview.NewGrid().
-		SetSize(8, 8, 8 * CELL_SIZE, 8 * CELL_SIZE).
+		SetColumns(8).SetRows(8).
 		SetBorders(true)
 		// AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false).
 		// AddItem(newPrimitive("Footer"), 2, 0, 1, 3, 0, 0, false)
 
 	for i := range 8 {
 		for j := range 8 {
-			grid.AddItem(menu, i, j, i * CELL_SIZE, j * CELL_SIZE, 0, 0, true)
+			grid.AddItem(newPrimitive("0"), i, j, 1, 1, 1, 1, false)
 		}
 	}
 
